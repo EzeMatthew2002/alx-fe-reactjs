e0dc2daefd136910c0c2eb7fc020169d971739c1
Ezebong Matthew <ezebongmatthew490@gmail.com>
2025-07-08 00:05:38 +0100

    My first React app With Alx

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,14 +1,12 @@
 import { useState } from "react";
 import WelcomeMessage from "./WelcomeMessage";
-import Header from "./Header";
+import Header from "./Header";    
 import Main from "./MainContent";
 import Footer from "./Footer";
 import UserProfile from "./UserProfile";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <div>
       <WelcomeMessage />
@@ -16,8 +14,11 @@ function App() {
       <Main />
       <Footer />
       <UserProfile name="Ezebong matthew" age={22} bio="A frontend developer" />
-      <UserProfile name="John Doe" age={30} bio="Tech enthusiast and traveler." />
-      
+      <UserProfile
+        name="John Doe"
+        age={30}
+        bio="Tech enthusiast and traveler."
+      />
     </div>
   );
 }
