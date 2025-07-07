76f2096fc871e1e44fdaae2ba864dce4d68625f6
Ezebong Matthew <ezebongmatthew490@gmail.com>
2025-07-08 00:27:47 +0100

    My first React app With Alx

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import WelcomeMessage from "./components/WelcomeMessage";
-import Header from "./components/Header";    
-import Main from "./components/MainContent";
+import Header from "./components/Header";
+import MainContent from "./components/MainContent";
 import Footer from "./components/Footer";
 import UserProfile from "./components/UserProfile";
 import "./App.css";
@@ -11,9 +11,9 @@ function App() {
     <div>
       <WelcomeMessage />
       <Header />
-      <Main />
+      <MainContent />
       <Footer />
-       <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
+      <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
     </div>
   );
 }
