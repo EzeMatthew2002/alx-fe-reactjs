1e09d573d87004f0499573578bc9196901fec635
EzeMatthew2002 <ezebongmatthew490@gmail.com>
2025-08-14 15:34:34 +0100

    Added a recipeForm

--- recipe-sharing-platform/src/components/AddRecipeForm.jsx
+++ recipe-sharing-platform/src/components/AddRecipeForm.jsx
@@ -8,6 +8,7 @@ const AddRecipeForm = () => {
     image: "",
     ingredients: [],
     instructions: [],
+    steps: []
   });
 
   const handleSubmit = (e) => {
@@ -30,6 +31,7 @@ const AddRecipeForm = () => {
       image: "",
       ingredients: [],
       instructions: [],
+      steps: []
     });
   };
 
