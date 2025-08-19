d27a4f3799c2a313c8b928a8cfee616314234a58
EzeMatthew2002 <ezebongmatthew490@gmail.com>
2025-08-19 20:56:15 +0100

    settingUp formik and yup

---
+++ form-handling-react/src/components/RegistrationForm.jsx
@@ -0,0 +1,85 @@
+import React, { useState } from "react";
+
+const RegistrationForm = () => {
+  const [userName, setUserName] = useState("");
+  const [email, setEmail] = useState("");
+  const [passWord, setPassword] = useState("");
+
+  const [userNameError, setUserNameError] = useState("");
+  const [emailError, setEmailError] = useState("");
+  const [passwordError, setPasswordError] = useState("");
+
+  function handleUserName(e) {
+    const value = e.target.value.trimStart();
+    setUserName(value);
+
+    if (value === "") {
+      setUserNameError("Please enter a name");
+    } else {
+      setUserNameError("");
+    }
+  }
+
+  function handleEmail(e) {
+    const value = e.target.value.trim();
+    setEmail(value);
+
+    if (!value.includes("@")) {
+      setEmailError("Please enter a correct email");
+    } else {
+      setEmailError("");
+    }
+  }
+
+  function handlePassword(e) {
+    const value = e.target.value.trim();
+    setPassword(value);
+
+    if (value === "") {
+      setPasswordError("Please enter a password");
+    } else {
+      setPasswordError("");
+    }
+  }
+
+  return (
+    <div>
+      <form>
+        <div>
+          <label htmlFor="username">UserName</label>
+          <input
+            type="text"
+            name="username"
+            value={userName}
+            onChange={handleUserName}
+          />
+          {userNameError && <p style={{ color: "red" }}>{userNameError}</p>}
+        </div>
+
+        <div>
+          <label htmlFor="email">Enter your email</label>
+          <input
+            type="email"
+            name="email"
+            value={email}
+            onChange={handleEmail}
+          />
+          {emailError && <p style={{ color: "red" }}>{emailError}</p>}
+        </div>
+
+        <div>
+          <label htmlFor="password">Enter your password</label>
+          <input
+            type="password"
+            name="password"
+            value={passWord}
+            onChange={handlePassword}
+          />
+          {passwordError && <p style={{ color: "red" }}>{passwordError}</p>}
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default RegistrationForm;
